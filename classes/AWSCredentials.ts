a79bce8ab73c446c5ad9b457c6c8c56d5921ba71
mmmaxwwwell <amclauss1@gmail.com>
2020-09-21 19:44:31 -0400

    removed bloat and bad ideas

--- classes/AWSCredentials.ts
+++
@@ -1,9 +0,0 @@
-export class AWSCredentials {
-  constructor(AWSAccessKeyId: string, AWSSecretKey: string) {
-    this.AWSAccessKeyId = AWSAccessKeyId
-    this.AWSSecretKey = AWSSecretKey
-  }
-
-  AWSAccessKeyId: string
-  AWSSecretKey: string
-}
