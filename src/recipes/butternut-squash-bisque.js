cecee72ddc1bdd912e4bb28044f2521098e40635
mmmaxwwwell <mmmaxwwwell@github.com>
2020-10-29 19:34:04 -0400

    removed dev recipe, added wine poached mahi mahi and butternut squash bisque

---
+++ src/recipes/butternut-squash-bisque.js
@@ -0,0 +1,85 @@
+module.exports = {
+    title: "Butternut Squash 'Bisque'",
+    slug: 'butternut-squash-bisque',
+    shortDescription: "Simple, vegan and full of fall flavors, this is a staple in our house.",
+    description: "With just 3 main ingredients and common spices, this is a family favorite that pairs well with may dishes. Bisque is in quotes because we limit the water content to achieve a similar consistency. It does not contain milk so it is not a true bisque.",
+    ingredients: [
+      {
+        quantity: 1,
+        units: 'whole',
+        item: 'butternut squash',
+        mods: ['cubed']
+      },
+      {
+        quantity: 1,
+        units: 'whole',
+        item: 'apple',
+        mods: ['cubed']
+      },
+      {
+        quantity: 1,
+        units: 'tsp',
+        item: 'bullion'
+      },
+      {
+        quantity: 1,
+        units: 'tbsp',
+        item: 'cinnamon',
+        mods: ['powdered']
+      },
+      {
+        quantity: 2,
+        units: 'whole',
+        item: 'cardamum pods',
+        mods: ['shelled']
+      },
+      {
+        quantity: 2,
+        units: 'whole',
+        item: 'clove buds',
+      },
+      {
+        quantity: '1/4',
+        units: 'tsp',
+        item: 'nutmeg',
+      },
+      {
+        quantity: '1/4',
+        units: 'tsp',
+        item: 'tumeric',
+        mods: ['powdered']
+      },
+    ],
+    instructions: [
+      "Add butternut squash, apple, bullion and powdered spices to instant pot with enough water to almost cover the ingredients. Add cardamum pods and cloves in a metal tea ball. Pressure cook for 25 mins on high.",
+      "When done, quick vent pressure and remove lid. Use a ladle to remove water until water no longer covers the squash and apple. Remove tea ball. Immersion blend until all the chunks are gone.",
+      "Set instant pot to sauté and bring to a boil to ensure food safety after the immersion blender."
+    ],
+    equipment:{
+      required:[
+        {
+            name: "8 Quart Instant Pot",
+            link: 'https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ',
+        },
+        {
+            name: "Immersion Blender",
+            link: 'https://www.amazon.com/AmazonBasics-Watt-Multi-Speed-Immersion-Blender/dp/B07Q12PJYP/ref=asc_df_B07Q12PJYP'
+        }
+      ],
+      recommended:[
+      ]
+    },
+    tags: [
+      "gluten-free",
+      "soy-free",
+      "shellfish-free",
+      "egg-free",
+      "meat-free",
+      "poultry-free",
+      "peanut-free",
+      "sugar-free",
+      'milk-free',
+      'vegan',
+      'vegetarian'
+    ]
+  }
